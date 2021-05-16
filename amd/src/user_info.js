7491b0b407875c67061a458ee0a7469b1be1ebfc
Richard Jones <richardnz@outlook.com>
2021-05-16 13:01:14 +1200

    Get student name into modal popup

--- amd/src/user_info.js
+++ amd/src/user_info.js
@@ -1,10 +1,11 @@
-import templates from 'core/templates';
+//import templates from 'core/templates';
 import log from 'core/log';
 
 export const get_info = () => {
     const lis = document.getElementsByClassName('block_modals_unmarked');
     const modal = document.getElementById('block_modals_myModal');
     const span = document.getElementsByClassName('block_modals_close')[0];
+    const content = document.querySelector('.block_model_details');
 
     span.onclick = function() {
         modal.style.display = "none";
@@ -12,10 +13,9 @@ export const get_info = () => {
 
     for (let li of lis) {
         li.onclick = function() {
-            var context = {student: '>' + li.innerHTML};
-            log.info('>' + li.innerHTML);
-            templates.render('block_modals/course_users', context);
             modal.style.display = "block";
+            log.info(content.childNodes[1].innerHTML);
+            content.childNodes[1].innerHTML += li.innerHTML;
         };
     }
 };
